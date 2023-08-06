aa09887cf0d123407e71a3f1a889ac8558a91ce2
Татьяна <ltasya72@gmail.com>
2023-08-07 01:08:48 +0300

    адаптирована reviews до 767

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,37 @@
 $(".nav__burger-btn").on("click", function () {
   $(".nav__list").toggleClass("menu--open");
 });
+
+// $(function () {
+$(".reviews__slider").slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false
+
+  //     responsive: [
+  //   {
+  //     breakpoint: 1024,
+  //     settings: {},
+  //   },
+  //   {
+  //     breakpoint: 640,
+  //     settings: {
+  //       arrows: false,
+  //     },
+  //   },
+  // ],
+});
+// });
+
+// const swiper = new Swiper(".swiper", {
+//   // effect: "coverflow",
+//   effect: 'fade',
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   autoplay: {
+//     delay: 4000,
+//     disableOnInteraction: true
+//   }
+// });
